8c9caedf1ad32327c8d57d28c5e1ad3a4b54c48b
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-11 16:05:30 +0100

    new

---
+++ src/Components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function header() {
+  return (
+    <header>
+    <div className='nav'>
+      <nav className='flex-box'>
+      <img src="./Assets/streamlit-logo-.png"/>
+      </nav>
+      
+    </div>
+    </header>
+  )
+}
+
+export default header
